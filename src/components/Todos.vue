0d9257e090e146a1748489d3fde354aa67677482
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2022-10-25 18:53:53 +0530

    delete todo

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,8 +1,22 @@
 <template>
-  <h3>Todos</h3>
-  <div class="todos">
-    <div :key="todo.id" v-for="todo in allTodos" class="todo">
-      {{ todo.title }}
+  <div>
+    <h3>Todos</h3>
+    <div class="legend">
+      <span>Double click to mark as complete</span>
+      <span> <span class="incomplete-box"></span> = Incomplete </span>
+      <span> <span class="complete-box"></span> = Complete </span>
+    </div>
+    <div class="todos">
+      <div
+  
+        v-for="todo in allTodos"
+        :key="todo.id"
+        class="todo"
+        v-bind:class="{ 'is-complete': todo.completed }"
+      >
+        {{ todo.title }}
+        <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -12,10 +26,11 @@ import { mapGetters, mapActions } from "vuex";
 
 export default {
   name: "Todos",
-  computed: mapGetters(["allTodos"]),
   methods: {
-    ...mapActions(["fetchTodos"]),
+    ...mapActions(["fetchTodos", "deleteTodo", "updateTodo"]),
+
   },
+  computed: mapGetters(["allTodos"]),
   created() {
     this.fetchTodos();
   },
